dc87a992c175dd08228c27f9cabd5bd58ff19d3a
brunovcunha <bruno.cunha@estudante.iftm.edu.br>
2024-09-17 15:00:53 -0300

    Adicionando validacao nos objetos Dto

--- src/main/java/com/brunov/demo_park_api/web/controller/UsuarioController.java
+++ src/main/java/com/brunov/demo_park_api/web/controller/UsuarioController.java
@@ -6,6 +6,7 @@ import com.brunov.demo_park_api.web.controller.dto.UsuarioCreateDto;
 import com.brunov.demo_park_api.web.controller.dto.UsuarioResponseDto;
 import com.brunov.demo_park_api.web.controller.dto.UsuarioSenhaDto;
 import com.brunov.demo_park_api.web.controller.dto.mapper.UsuarioMapper;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,7 +21,7 @@ public class UsuarioController {
     private final UsuarioService usuarioService;
 
     @PostMapping
-    public ResponseEntity<UsuarioResponseDto> create(@RequestBody UsuarioCreateDto createDto) {
+    public ResponseEntity<UsuarioResponseDto> create(@Valid @RequestBody UsuarioCreateDto createDto) {
         Usuario user = usuarioService.salvar(UsuarioMapper.toUsuario(createDto));
 
         return ResponseEntity.status(201).body(UsuarioMapper.toDto(user));
@@ -35,7 +36,7 @@ public class UsuarioController {
 
 
     @PatchMapping("/{id}")
-    public ResponseEntity<Void> updatePassword(@RequestBody UsuarioSenhaDto dto, @PathVariable Long id) {
+    public ResponseEntity<Void> updatePassword(@Valid @RequestBody UsuarioSenhaDto dto, @PathVariable Long id) {
         Usuario user = usuarioService.editarSenha(id,
                 dto.getSenhaAtual(),
                 dto.getNovaSenha(),
--- src/main/java/com/brunov/demo_park_api/web/controller/dto/UsuarioCreateDto.java
+++ src/main/java/com/brunov/demo_park_api/web/controller/dto/UsuarioCreateDto.java
@@ -1,6 +1,9 @@
 package com.brunov.demo_park_api.web.controller.dto;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -8,7 +11,14 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+
 public class UsuarioCreateDto {
+
+    @NotBlank
+    @Email(message = "formato do email invalido", regexp = "^[a-z0-9.*-]+@[a-z0-9.-]+\\.[a-z]{2,}$")
     private String username;
+
+    @NotBlank
+    @Size(min = 6, max = 6)
     private String password;
 }
